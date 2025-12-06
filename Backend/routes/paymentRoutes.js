406ce5402457833773aa7bba947ab53ffa43d5e5
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-12-07 04:02:29 +0530

    completed payment part

--- Backend/routes/paymentRoutes.js
+++ Backend/routes/paymentRoutes.js
@@ -1,7 +1,13 @@
 const { Router } = require('express');
-const { createOrder } = require('../controllers/paymentController');
+const { createOrder, verifyPayment } = require('../controllers/paymentController');
 const router = Router();
+const auth = require('../middleware/authMiddleware');
+const iscompany = require('../middleware/isCompany');
+// router.use(auth);
+// router.use(iscompany);
 
 router.post('/create-order', createOrder);
+router.post('/verify-payment', verifyPayment);
+
 
 module.exports = router;
