0746fe141758000e25cbe7b95814965dce6e8cb4
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-12-11 23:34:58 +0530

    Add route for getting farmer payment details
    Added a route to get wallet details for farmers.

--- Backend/routes/walletRoutes.js
+++ Backend/routes/walletRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const walletController = require('../controllers/walletController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+// Getting Farmer Payment Details
 router.get('/', authMiddleware, walletController.getWalletDetails);
 
 module.exports = router;
